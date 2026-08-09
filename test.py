43618ef71becdb05fc6ab5861abd15aa53334db9
WaddingsNZ <wxf2218@autuni.ac.nz>
2026-08-10 09:26:46 +1200

    Ben Update 2
    This is my second github commit from my school account

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ print("hello testing")
 
 print("This is the change from your friend")
 print("Ben Present 2026-08-10")
+print("This is my second commit to github on my school account")
 
 print("New commit")
